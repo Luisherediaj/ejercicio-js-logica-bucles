b9dc23603a7b08835fcb8138e08a196f7895f4d0
Luis <luis.here.j@gmail.com>
2021-05-03 09:50:45 +0200

    Resuelto ejercicio 15-5
    Resuelto Mostrar en el navegador todos los números divisores de un número introducido en un prompt.

--- 15-ejercicio5.js
+++ 15-ejercicio5.js
@@ -4,10 +4,16 @@
 Mostrar en el navegador todos los números divisores de un número introducido en un prompt.
 */
 
-var n1 = prompt("Escribe un número");
-var i;
-for (i=2;i < n1/2; i++) {
-if (n1 % i === 0) {
-document.write(i,", ");
+let numero = parseInt(prompt("Escoge los divisores de un numero"));
+
+document.write("<h2>Estos son los divisores "+numero+"</h2>");
+for (let i = numero; i >= 1; i--) {
+    
+   
+    if(numero%i == 0){
+        document.write("<p>"+i+"</p>");
+    }
+
+    
 }
-}
+
