9f5cb6c6ebcb80a6596293603e3ba825fdc1f1c3
Luis <luis.here.j@gmail.com>
2021-05-03 11:28:40 +0200

    Ejercicio 7 resuelto, inicio ejercicio 8

--- 17-ejercicio7.js
+++ 17-ejercicio7.js
@@ -3,15 +3,21 @@ Tabla de multiplicar de un número introducido por pantalla
 */
 
 // Pide introducir el número sobre el que generar la tabla
-var input = parseInt(prompt("Introduce el número sobre el cual crear la tabla"));
+// var input = parseInt(prompt("Introduce el número sobre el cual crear la tabla"));
 
-// Introduce encabezados de la tabla
-var resultado = "<TR><TH>Multiplicación</TH><TH>Resultado</TH></TR>";
+// // Introduce encabezados de la tabla
+// var resultado = "<TR><TH>Multiplicación</TH><TH>Resultado</TH></TR>";
 
-// Bucle que recorre hasta multiplicar por 10
-for (i=0;i<=10;i++) {
-    resultado += '<tr><td>' + input + 'x' + i + '</td><td>' + (input * i) + '</td></tr>';
+// // Bucle que recorre hasta multiplicar por 10
+// for (i=0;i<=10;i++) {
+//     resultado += '<tr><td>' + input + 'x' + i + '</td><td>' + (input * i) + '</td></tr>';
+// }
+
+// // Muestra el resultado por pantalla
+// document.getElementById('tabla').innerHTML = resultado;
+
+let numero = parseInt(prompt('Introduzca un número'));
+
+for (let i = 1; i <= 10; i++) {
+    document.write(`${numero} x ${i} = ${numero*i} <br>`);
 }
-
-// Muestra el resultado por pantalla
-document.getElementById('tabla').innerHTML = resultado;
--- 18-ejercicio8.js
+++ 18-ejercicio8.js
@@ -6,3 +6,35 @@ Si metemos uno mal que nos los vuelva a pedir.
 En el cuerpo de la página y por la consola el resultado
 de sumar, restar, multiplicar y dividir esas dos cifras.
 */
+
+function calculaNumeros(){
+    do {
+        var num1 = parseInt(prompt('Introduce un número'));
+        var num2 = parseInt(prompt('Introduce otro numero'));
+    } while (isNaN(num1) || isNaN(num2));
+
+    //mostrar en pantalla
+    document.write(`${numero1} + ${numero2} = ${suma(num1, num2)} <br>`);
+    document.write(`${numero1} - ${numero2} = ${resta(num1, num2)} <br>`);
+    document.write(`${numero1} x ${numero2} = ${multiplica(num1, num2)} <br>`);
+    document.write(`${numero1} / ${numero2} = ${division(num1, num2)} <br>`);
+
+}
+
+function suma(numero1, numero2){
+    return numero1+numero2;
+}
+
+function resta(numero1, numero2){
+    return numero1-numero2;
+}
+
+function multiplica(numero1, numero2){
+    return numero1*numero2;
+}
+
+function division(numero1, numero2){
+    return numero1/numero2;
+}
+
+calculaNumeros();
