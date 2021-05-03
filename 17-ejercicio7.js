f1307ed74af4a077a3d1dd4328b58c96286ca70f
Luis <luis.here.j@gmail.com>
2021-05-03 10:28:26 +0200

    intento ejercicio 17-7
    intento de que aparezca la tabla de multiplicar

--- 17-ejercicio7.js
+++ 17-ejercicio7.js
@@ -1,3 +1,17 @@
 /*
 Tabla de multiplicar de un número introducido por pantalla
 */
+
+// Pide introducir el número sobre el que generar la tabla
+var input = parseInt(prompt("Introduce el número sobre el cual crear la tabla"));
+
+// Introduce encabezados de la tabla
+var resultado = "<TR><TH>Multiplicación</TH><TH>Resultado</TH></TR>";
+
+// Bucle que recorre hasta multiplicar por 10
+for (i=0;i<=10;i++) {
+    resultado += '<tr><td>' + input + 'x' + i + '</td><td>' + (input * i) + '</td></tr>';
+}
+
+// Muestra el resultado por pantalla
+document.getElementById('tabla').innerHTML = resultado;
