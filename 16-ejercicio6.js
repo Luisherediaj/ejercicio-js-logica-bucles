f667afacad8640eedaee0954eacb1ca9e365c046
Luis <luis.here.j@gmail.com>
2021-05-03 09:56:36 +0200

    Resuelto el ejercicio 16-6
    Resuelto el de "Se le pide a un usuario por pantalla (prompt) un número y el programa nos tiene que mostrar si es par o impar.
    Si el número introducido no es válido nos pide de nuevo el número."

--- 16-ejercicio6.js
+++ 16-ejercicio6.js
@@ -6,3 +6,15 @@ Si el número introducido no es válido nos pide de nuevo el número.
 
 */
 
+do {
+    var numero = parseInt(prompt("Numero por pantalla:"));
+} while (isNaN(numero));
+
+
+if(numero%2 == 0){
+    
+    document.write("<p>El número "+numero+" es PAR</p>");
+}else{
+   
+    document.write("<p>El número "+numero+" es IMPAR</p>");
+}
